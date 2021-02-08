1dca394b4fdf9e824dcda96e22b8c919a480aa31
KenanBolat <kenan23@gmail.com>
2021-02-08 13:30:23 +0300

    Make single string line

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ start = datetime.datetime.now()
 process_path = "./files"
 
 # Add/update file flags
-file_flags = ['N15', 'N18', 'N19', 'M01']
+# file_flags = ['N15', 'N18', 'N19', 'M01']
 
 # Define pattern
-pattern = r"(^[x]{4}_OSV_(" + "|".join(file_flags) + ")_\d{8})|(^tle_\d{8}.txt)"
+# pattern = r"(^[x]{4}_OSV_(" + "|".join(file_flags) + ")_\d{8})|(^tle_\d{8}.txt)"
+pattern = r"(^[x]{4}_OSV_(N15|N18|N19|M01)_\d{8})|(^tle_\d{8}.txt)"
+
 
 # apply filtering
 files = [row for row in os.listdir(process_path) if re.search(pattern, row)]
