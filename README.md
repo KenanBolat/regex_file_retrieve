578986c10b762d7f55273e0bd4a9f65bc883cb32
KenanBolat <kenan23@gmail.com>
2021-02-08 11:50:07 +0300

    Add comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # regex_file_retrieve
+Simple Regex example that filters out multiple files according to the given mathcing pattern. 
+ 
--- main.py
+++ main.py
@@ -2,10 +2,23 @@ import re
 import os
 import datetime
 import pprint
+
+# Initiation Time
 start = datetime.datetime.now()
+
+# Define process path
 process_path = "./files"
+
+# Add/update file flags
 file_flags = ['N15', 'N18', 'N19', 'M01']
-pattern = r"(^[x]{4}_OSV_("+"|".join(file_flags)+")_\d{8})|(^tle_\d{8}.txt)"
+
+# Define pattern
+pattern = r"(^[x]{4}_OSV_(" + "|".join(file_flags) + ")_\d{8})|(^tle_\d{8}.txt)"
+
+# apply filtering
 files = [row for row in os.listdir(process_path) if re.search(pattern, row)]
+
 pprint.pprint(files)
+# Script run time
 end = datetime.datetime.now()
+print("Initiation time:", start, " Duration:", end - start)
